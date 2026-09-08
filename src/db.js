d1866e89e8cb96f61b637a90cf207822e2af9a58
Zhizhuang Qiang <z.qiang@outlook.com>
2026-09-08 15:01:21 +0200

    fixed ssl issue

--- src/db.js
+++ src/db.js
@@ -20,7 +20,9 @@ if (!process.env.DATABASE_URL) {
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: sslOptions(),
+  ssl: {
+	rejectUnauthorized: false
+  },
   max: 10,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 10000,
